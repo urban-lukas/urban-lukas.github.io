23947fa5b28ff20ea9b68eab77774c930b32cfbc
ezyn <ezyn69@gmail.com>
2022-08-06 18:29:43 +0200

    Page upload

---
+++ js/index_script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+    // when window width is >= 680px
+    720: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    // when window width is >= 900px
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    }
+  }
+});
